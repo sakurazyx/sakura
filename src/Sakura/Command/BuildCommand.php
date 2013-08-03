700befe5b87b0163b96353bf89ef5b6248998d83
Alberto Piu <alberteddu@gmail.com>
2013-08-03 20:44:14 +0200

    First commit

---
+++ src/Sakura/Command/BuildCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sakura\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BuildCommand extends Command {
+    /**
+     * Configure build command
+     */
+    public function configure() {
+        $this->setName("build")
+            ->setDescription("Build website");
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    public function execute(InputInterface $input, OutputInterface $output) {
+
+    }
+}
---
+++ src/Sakura/Sakura.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sakura;
+
+/**
+ * Class Sakura
+ *
+ * @package Sakura
+ */
+class Sakura {
+    const APPLICATION_NAME = "Sakura";
+    const APPLICATION_VERSION = "0.0.1";
+}
