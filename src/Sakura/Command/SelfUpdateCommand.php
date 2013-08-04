587b7614e08319b68ab8473674c241dd6cb68f5c
Alberto Piu <alberteddu@gmail.com>
2013-08-04 11:36:41 +0200

    Added self-update command

--- src/Sakura/Command/SelfUpdateCommand.php
+++ src/Sakura/Command/SelfUpdateCommand.php
@@ -3,6 +3,10 @@
 namespace Sakura\Command;
 
 use Sakura\Sakura;
+
+use Herrera\Phar\Update\Manager;
+use Herrera\Phar\Update\Manifest;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -15,12 +19,15 @@ use Symfony\Component\Console\Output\OutputInterface;
  * @package Sakura\Command
  */
 class SelfUpdateCommand extends Command {
+    const MANIFEST_FILE = "http://alberteddu.github.com/sakura/manifest.json";
+
     public function configure() {
         $this->setName('self-update')
             ->setDescription('Update ' . Sakura::APPLICATION_NAME . ' to the last version');
     }
 
     public function execute(InputInterface $input, OutputInterface $output) {
-
+        $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
+        $manager->update($this->getApplication()->getVersion(), true);
     }
 }
