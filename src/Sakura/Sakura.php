f5a4377385c01fc770a5a72c08b6f779c1b7472c
Alberto Piu <alberteddu@gmail.com>
2013-08-04 20:52:39 +0200

    Added Sakura

--- src/Sakura/Sakura.php
+++ src/Sakura/Sakura.php
@@ -10,4 +10,16 @@ namespace Sakura;
 class Sakura {
     const APPLICATION_NAME = "Sakura";
     const APPLICATION_VERSION = "0.0.1";
+
+    /** @var string Directory path */
+    protected $directory;
+
+    /**
+     * Create Sakura instance
+     *
+     * @param $directory
+     */
+    public function __construct($directory) {
+        
+    }
 }
